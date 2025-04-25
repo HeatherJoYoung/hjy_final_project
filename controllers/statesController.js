f53a6327787c64e2a23257d3cbd00ecc8e97b296
hjyoung <hjy.earth@gmail.com>
2025-04-25 13:19:45 -0600

    adds contig filter for states GET request

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -1,22 +1,25 @@
 const data = require('../models/states.json')
 const State = require('../models/State');
 
-/* const getAllStates = async (req, res) => {
-	const states = await State.find();
-	if (!states) {
-		return res.status(204).json({ 'message': 'No states found.' });
-	}
-	res.json(states.map(state => {
-		return {
-			code: state.stateCode,
-			funfacts: state.funfacts
-		}
-	}));
-} */
+const contigStates = ['AL','AZ','AR','CA','CO','CT','DE','FL','GA','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
+const nonContigStates = ['AK', 'HI']
 
 const getAllStates = async (req, res) => {
+	const contig = req.query.contig;
+	let states = data;
+
+	if (contig) {
+		if (contig === 'true') {
+			states = data.filter(state => contigStates.includes(state.code));
+		} else if (contig === 'false') {
+			states = data.filter(state => nonContigStates.includes(state.code));
+		} else {
+			return res.status(400).json({ 'message': 'Invalid query parameter. Use "true" or "false".' });
+		}
+	}
+	console.log(`number of states returned: ${states.length}`)
 	const stateFacts = await State.find();
-	res.json(data.map(state => ({ ...state, funfacts: stateFacts.find(fact => fact.stateCode === state.code)?.funfacts })));
+	res.json(states.map(state => ({ ...state, funfacts: stateFacts.find(fact => fact.stateCode === state.code)?.funfacts })));
 }
 
 module.exports = {
